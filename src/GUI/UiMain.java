6bc2cf4d83fd864c6863df2fc27339a14b7c7126
josedavms13 <josedav0ms13@gmail.com>
2022-05-01 20:16:26 -0500

    Adding set up file

--- src/GUI/UiMain.java
+++ src/GUI/UiMain.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import javax.swing.*;
+
 import GUI.Pages.FirstPage;
 import GUI.Pages.MessageDisplay;
 import GUI.assets.ColorPallette;
@@ -20,25 +21,56 @@ import static java.lang.Thread.sleep;
 
 public class UiMain extends JFrame implements ActionListener {
 
-    private final String adviserTime = "20:50";
-    private final String closingTime = "21:00";
+    private boolean checkAlways = false;
+    private String adviserTime = "20:50";
+    private String closingTime = "21:00";
     private SubmitButton submitButton = new SubmitButton("submit");
     private MessageDisplay messageLabel;
 
-    public UiMain() {
-        try {
-            File myFile = new File("saverSetUp.txt");
-            Scanner myReader = new Scanner(myFile);
-            while (myReader.hasNextLine()) {
+    private final String textFile = "C:\\Users\\josed\\Documents\\DESARROLLO\\JAVA\\FutureSaver\\saverSetUpt.txt";
 
+    private File myFile;
+
+    private Scanner myReader;
+
+    public UiMain() {
+
+        try {
+            sleep(10000);
+            myFile = new File(textFile);
+            myReader = new Scanner(myFile);
+            while (myReader.hasNextLine()) {
+                try {
+                    adviserTime = myReader.nextLine();
+                    closingTime = myReader.nextLine();
+                } catch (Exception e) {
+                    System.out.println("default");
+
+                }
+                String checkAlwaysString;
+                try {
+                    checkAlwaysString = myReader.nextLine();
+                } catch (Exception e) {
+                    checkAlwaysString = "false";
+                }
+                checkAlways = checkAlwaysString.length() > 0 && Boolean.parseBoolean(checkAlwaysString);
+                System.out.println("Adviser Time: " + adviserTime);
+                System.out.println("Closing Time: " + closingTime);
+                System.out.println("check Always: " + checkAlways);
             }
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("File failed");
+            this.timer();
+            adviserTime = "20:50";
+            closingTime = "21:00";
+        } catch (InterruptedException e) {
+            System.out.println("Failed sleep");
         }
         this.timer();
     }
 
     public void timer() {
+        myReader.close();
         boolean isRunning = true;
         boolean isAdvised = true;
         while (isRunning) {
@@ -50,12 +82,12 @@ public class UiMain extends JFrame implements ActionListener {
 
                 String currentTime = dtf.format(now);
                 System.out.println(currentTime);
-                if(currentTime.equals(this.adviserTime) && isAdvised) {
+                if (currentTime.equals(this.adviserTime) && isAdvised) {
                     this.advise();
                     isAdvised = false;
                 }
 
-                if(currentTime.equals(this.closingTime)) {
+                if (currentTime.equals(this.closingTime)) {
                     Runtime.getRuntime().exec("shutdown /s");
                 }
 
